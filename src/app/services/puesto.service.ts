bf8e14adbdb5ea62a5995fe8950484cd7b16b202
Esteban Medina <jmedina@advisoryservicesg.tech>
2023-10-30 12:27:26 -0600

    Actualización service puesto

--- src/app/services/puesto.service.ts
+++ src/app/services/puesto.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
 import { addDoc, collection } from 'firebase/firestore';
+import { collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import Catalog from 'src/interfaces/catalog.interface';
 import Puesto from 'src/interfaces/position.interface';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class PuestoService {
 
   constructor(private firestore:Firestore) {
@@ -14,6 +18,11 @@ export class PuestoService {
    addPosition(position:Puesto){
       const positionRef = collection(this.firestore, 'positions')
       return addDoc(positionRef, position)
+   }
+
+   getPositions():Observable<Puesto[]>{
+    const catalogRef = collection(this.firestore, 'catalogs')
+      return collectionData(catalogRef, {idField:'id'}) as Observable<Puesto[]>
    }
 
    
